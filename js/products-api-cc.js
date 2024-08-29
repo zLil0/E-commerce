1c96661e7c32ea2804db336af5e1bbff00b445b9
zLilo <murilofm.social@gmail.com>
2024-08-29 15:23:32 -0300

    Product page/ pagination

--- js/products-api-cc.js
+++ js/products-api-cc.js
@@ -1,38 +1,21 @@
-const cardsContainer = document.querySelector('#home-main-content')
-const allProductsUrl = 'https://dummyjson.com/products?limit=33'
+const cardsContainer = document.querySelector('#cards-container')
+let page = 0
 
 
-const fetchAllProducts = async () => {
-  const response = await fetch(allProductsUrl)
+const fetchProducts = async (p) => {
+  const response = await fetch(`https://dummyjson.com/products?limit=24&skip=${24 * page}`)
   const data = await response.json()
   return data.products
 }
 
 const calcPrice = (product) => {
-  return price = product.price * 1 - (product.discountPercentage/100)
+  return price = product.price * (1 - (product.discountPercentage / 100))
 }
 
-const showAllCards = (arr) => {
+const showProducts = (arr) => {
+  cardsContainer.innerHTML = ''
   arr.forEach((product) => {
-    if(product.discountPercentage <= "0.5"){
-      cardsContainer.innerHTML += `
-      <div class="product-card" onclick="goToProductPage(${product.id})">
-        <img src="${product.thumbnail}" alt="${product.title}" class="card-thumbnail">
-        <div class="card-text">
-          <header class="card-header"><h3 class="product-name">${product.title}</h1></header>
-            <p class="card-body">
-              <span class="price">$ ${calcPrice(product).toFixed(2)}</span>
-            </p>
-            <div class="rating-container">
-              <img src="assets/star-rating.svg" alt="star">
-              <span class="rating">${product.rating}</span>
-            </div>
-          </div>
-      </div>
-    `
-    }
-    else {
-      cardsContainer.innerHTML += `
+    cardsContainer.innerHTML += `
       <div class="product-card" onclick="goToProductPage(${product.id})">
         <img src="${product.thumbnail}" alt="${product.title}" class="card-thumbnail">
         <div class="card-text">
@@ -42,24 +25,95 @@ const showAllCards = (arr) => {
               was <span class="old-price">$${product.price}</span>
             </p>
             <div class="rating-container">
-              <img src="assets/star-rating.svg" alt="star">
+              <img src="assets/star-rating0.svg" alt="star">
               <span class="rating">${product.rating}</span>
             </div>
           </div>
       </div>
     `
-    }
+
   })
+  document.querySelector('#page-number').innerHTML = page + 1
 }
 
-const getAllProducts = async () => {
-  const products = await fetchAllProducts()
-  showAllCards(products)
+const productsPagination = async (move) => {
+  if (move === 'next') page++
+  else if (move === 'prev') page--
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+  const products = await fetchProducts(page)
+  showProducts(products)
 }
 
-getAllProducts()
+const goToProductPage = async (i) => {
+  const response = await fetch(`https://dummyjson.com/products/${i}`)
+  const product = await response.json()
+  document.querySelector('#home-main-content').innerHTML = `
+  <div id="product-page">
+  <img src="${product.thumbnail}"
+        alt="${product.title}" class="card-thumbnail">
+      <div class="card-text">
+        <header class="card-header">
+          <h2 id="product-name">${product.title}</h1>
+            <h3 id="product-brand">${product.brand}</h3>
+        </header>
+        <p id="product-description">${product.description}</p>
+        <div id="price-rating-container">
+          <div id="price-info">
+            <p><span id="discount">-%${product.discountPercentage} </span><span id="price">$${calcPrice(product).toFixed(2)}</span></p>
+            <p id="old-price">was <span>${product.price}</span></p>
+          </div>
+          <div class="rating-container">
+            <img src="assets/star-rating0.svg" alt="star">
+            <span class="rating">${product.rating}</span>
+          </div>
+        </div>
+        <div id="button-container">
+          <button id="add-to-cart">Add-to-Cart</button>
+          <p>Stock: <span id="stock-count">${product.stock}</span></p>
+        </div>
+      </div>
+  `
+  showReviews(product)
+}
 
-const goToProductPage = (i) =>{
-  window.location.href ='product.html'
+const reviewStars = (rating) =>{
+  let stars = [0,0,0,0,0]
+  for(let i = 0; i<rating; i++){
+    stars[i] = 1
+  }
+  return stars
+}
+
+const showReviews = (product) => {
+  const main = document.querySelector('#home-main')
+  main.innerHTML += `
+  <div id="comment-section">
+      <h2>Comments</h2>
+  </div>
+  `
+  const commentSection = document.querySelector('#comment-section')
+  product.reviews.forEach((review)=>{
+    const stars = reviewStars(review.rating)
+    commentSection.innerHTML += `
+    <div class="comment-card">
+        <header class="comment-header">
+          <h3 class="comment-name">${review.reviewerName}</h3>
+          <div class="stars-container">
+            <img src="assets/star-rating${stars[0]}.svg" alt="star">
+            <img src="assets/star-rating${stars[1]}.svg" alt="star">
+            <img src="assets/star-rating${stars[2]}.svg" alt="star">
+            <img src="assets/star-rating${stars[3]}.svg" alt="star">
+            <img src="assets/star-rating${stars[4]}.svg" alt="star">
+          </div>
+        </header>
+        <h4 class="comment-email">${review.reviewerEmail}</h4>
+        <p class="comment-description">${review.comment}</p>
+        <p class="comment-date">${review.date.split('T')[0]}</p>
+      </div>
+    `
+  })
 }
 
